5e49cbe522bc03b1b098046bf09ab1ce50202a69
ivan <ivan@debian8.debian8.ru>
2017-07-03 16:50:25 +0300

    add jest

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
 
 import program from 'commander';
+import compareFiles from '../compare-files';
 
-program.version('0.0.1')
+program.version('0.0.3')
   .usage('[options] <firstConfig> <secondConfig>')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'Output format')
   .parse(process.argv);
+
+export default compareFiles;
