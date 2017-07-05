73caf686884f8b891b721c4ebd76a18d944b6205
ivan <ivan@debian8.debian8.ru>
2017-07-05 10:17:40 +0300

    refactor

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ install:
 	npm install
 
 start:
-	npm run babel-node -- src/bin/gendiff.js __tests__/compare-json-before __tests__/compare-json-after
+	npm run babel-node -- src/bin/gendiff.js __tests__/compare/json/before.json __tests__/compare/json/after.json
 
 publish:
 	npm publish
--- __tests__/compare.test.js
+++ __tests__/compare.test.js
@@ -2,8 +2,8 @@ import gendiff from '../src';
 import fs from 'fs';
 
 test('Compare JSON files', () => {
-  const fileBefore = '__tests__/compare-json-before';
-  const fileAfter = '__tests__/compare-json-after';
-  const result = fs.readFileSync('__tests__/compare-json-result').toString();
+  const fileBefore = '__tests__/compare/json/before.json';
+  const fileAfter = '__tests__/compare/json/after.json';
+  const result = fs.readFileSync('__tests__/compare/json/result.txt').toString();
   expect(gendiff(fileBefore, fileAfter)).toBe(result);
 });
